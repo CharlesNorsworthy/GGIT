5d675ac1df034ca36b9c38bb6cafb1a441aae1d6
KChampagne8811 <kaleb.champagne@selu.edu>
2018-09-26 22:58:20 -0500

    Updated Const to include database labels, relationships, and node properties

--- src/graphTool/Const.java
+++ src/graphTool/Const.java
@@ -1,4 +1,10 @@
+/**
+ * Const holds variables used throughout GGIT/graphTool
+ */
 public class Const {
+    /**
+     * Node Properties
+     */
     public static final String UUID = "id";
 
     public static final String LONGITUDE = "longitude";
@@ -6,4 +12,18 @@ public class Const {
     public static final String LATITUDE = "latitude";
 
     public static final String NAME = "name";
+
+    /**
+     * Database Labels
+     */
+    public static final String OBSERVATION_LABEL = "OBSERVATION";
+
+    public static final String KNOWLEDGE_LABEL = "KNOWLEDGE";
+
+    /**
+     * Database Relationships
+     */
+    public static final String RELATE_ROOT_OBS = "Observation";
+
+    public static final String RELATE_OBS_KNOW = "Knowledge";
 }
