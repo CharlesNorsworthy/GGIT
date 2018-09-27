701c4822080e840817154c9e7999cb009fa479f3
Patrick <pataran@gmail.com>
2018-09-27 16:56:43 -0500

    Rearranged and refactored Util
    Create Nodes fefactor
    Connect Relationships refacter

--- src/graphTool/GraphApi.java
+++ src/graphTool/GraphApi.java
@@ -1,17 +1,13 @@
 package graphTool;
 
-<<<<<<< HEAD
 import org.neo4j.graphdb.Node;
 
-=======
->>>>>>> 12f1d15a1b2be46d153f0793ee21072665952df6
 /**
  * The GraphApi class will handle the outward facing api.
  * Possibly utilze Swagger
  */
 public class GraphApi {
 
-<<<<<<< HEAD
     graphTool.DbUtils database;
 
     public GraphApi(){
@@ -82,40 +78,7 @@ public class GraphApi {
 
     }
 
-    public void deleteAllRootsOfProperty(String prop, String propVal)
-    {
+    public void deleteAllRootsOfProperty(String prop, String propVal) {
         database.deleteNodes("Root", prop, propVal);
-=======
-    public void createObservation() {
-
-    }
-
-    public void readObservation() {
-
-    }
-
-    public void updateObservation() {
-
-    }
-
-    public void deleteObservation() {
-
-    }
-
-    public void createKnowledge() {
-
-    }
-
-    public void readKnowledge() {
-
-    }
-
-    public void updateKnowledge() {
-
-    }
-
-    public void deleteKnowledge() {
->>>>>>> 12f1d15a1b2be46d153f0793ee21072665952df6
-
     }
 }
--- src/main/java/GGIT.java
+++ src/main/java/GGIT.java
@@ -11,82 +11,6 @@ public class GGIT
         //executeCmd(args);
     }
 
-<<<<<<< HEAD
-import java.util.Scanner;
-
-    public class GGIT
-    {
-
-
-        public static void main(String args[]) {
-
-            menu();
-
-            Scanner sc = new Scanner(System.in);
-            String input = sc.nextLine();
-
-            executeCmd(input);
-        }
-
-        private static void executeCmd(String input){
-            graphTool.GraphApi database;
-            database = new graphTool.GraphApi();
-            try {
-                Scanner sc = new Scanner(System.in);
-                menu();
-                switch (input) {
-                    case "init":
-
-////
-//                        database.createRelationship("Observation","thermal","Knowledge","sonarData");
-//                        database.createRelationship("Root","root","Observation","thermal");
-//                        database.createObservation("thermal");
-//                        database.createObservation("flight");
-//                        database.showObservationByType("thermal");
-//                        database.showObservationByType("flight");
-//                        database.showRoot("root");
-//
-
-                    case "clone":
-
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-
-                    case "add":
-
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "commit":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "push":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "status":
-                        System.out.println("Please enter node,label,and property you want to select separated by commas");
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "remote":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "checkout":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "branch":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "pull":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "merge":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "diff":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "log":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "fetch":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "reset":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    case "grep":
-                        throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
-                    default:
-                        System.out.println("The user must enter a command");
-                }
-            } catch (UnsupportedOperationException exception) {
-                System.out.println(exception);
-=======
     private static void executeCmd(String args[]){
         try {
             String input = args[0];
@@ -125,7 +49,6 @@ import java.util.Scanner;
                     throw new UnsupportedOperationException("The " + input + " command is not currently supported.");
                 default:
                     System.out.println("The user must enter a command");
->>>>>>> 12f1d15a1b2be46d153f0793ee21072665952df6
             }
         } catch (UnsupportedOperationException e) {
             System.out.println(e);
