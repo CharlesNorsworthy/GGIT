7d2dc48a12e1e5e4ae374290ce4939f02566417a
KChampagne8811 <kaleb.champagne@selu.edu>
2018-09-26 22:52:11 -0500

    Added DbOps to handle calls between user input interface and graph database. Const file will keep track of our variable names used throughout project.

---
+++ src/graphTool/Const.java
@@ -0,0 +1,9 @@
+public class Const {
+    public static final String UUID = "id";
+
+    public static final String LONGITUDE = "longitude";
+
+    public static final String LATITUDE = "latitude";
+
+    public static final String NAME = "name";
+}
